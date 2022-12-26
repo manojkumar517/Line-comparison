bbaaf233efa8ddf5dd54b5823719e9b9bb860241
manojkumar <amanojkumar517@gmail.com>
2022-12-26 10:26:42 +0530

    ADDED: code to print length using co-ordinates

--- Line_comparison.java
+++ Line_comparison.java
@@ -1,7 +1,23 @@
 package com.bridzlabs;
 
+import java.util.Scanner;
+
 public class Line_comparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to line comparison computation");
+            // TODO Auto-generated method stub
+            System.out.println("Welcome to Line Comparison Computation Program");
+            Scanner scan = new Scanner(System.in);
+            float x1, y1, x2, y2, length1;
+
+            System.out.println(" Enter point for x1 and y1");
+            x1 = scan.nextFloat();
+            y1 = scan.nextFloat();
+
+            System.out.println(" Enter points for x2 and y2 ");
+            x2 = scan.nextFloat();
+            y2 = scan.nextFloat();
+
+            length1 = (float) Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+            System.out.println("Length of line one is " + length1);
     }
 }
