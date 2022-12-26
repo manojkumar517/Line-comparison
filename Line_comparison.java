d8799bcf3c61bfc03b002b189654d03e1775e225
manojkumar <amanojkumar517@gmail.com>
2022-12-26 09:09:42 +0530

    ADDED: code to print Welcome to line comparison computation

---
+++ Line_comparison.java
@@ -0,0 +1,7 @@
+package com.bridzlabs;
+
+public class Line_comparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation");
+    }
+}
